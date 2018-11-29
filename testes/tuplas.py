314ba545fa4e710d1889927a74c804a2571134a2
Diego Souza <diego@diegosouza.com.br>
2018-11-29 03:03:48 +0000

    novos testes

---
+++ testes/tuplas.py
@@ -0,0 +1,11 @@
+tuplas=("diego","souza","python")
+print(tuplas)
+print(tuplas[0])
+print(tuplas[0:2])
+print(len(tuplas))
+print(tuplas+tuplas)
+print(tuplas*5)
+print("python" in tuplas)
+lista=[1,4,"diego"]
+tuplas2=tuple(lista)
+print(tuplas2)
